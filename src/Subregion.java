10610d2f807f677e56be3dfce8a169f62b81040a
Timothy Ernst <Timothy Ernst>
2015-11-10 17:03:57 -0800

    Added subregion class

--- src/Subregion.java
+++ src/Subregion.java
@@ -2,7 +2,13 @@
 
 public class Subregion {
     Polygon region;
+    String name;
+    String regionName;
+    
+    public Subregion(String regionName ){
+      this.name = regionName;
 
+    }
 
     public void addBoundry(double x, double y){
       region.addPoint(x,y);
